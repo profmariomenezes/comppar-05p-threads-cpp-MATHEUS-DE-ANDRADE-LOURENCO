b568565702e9b7d04c9787c1791a11634e6cbe51
MATHEUS-DE-ANDRADE-LOURENCO <10419691@mackenzista.com.br>
2025-03-09 15:48:40 +0000

    Finalizado

--- col.cpp
+++ col.cpp
@@ -5,14 +5,16 @@
 # include <random>
 # include <chrono>
 
-void multiplicaLinha(int m1[500][500], int m2[500][500], int resultado[500][500], int linha) {
-    for(int j = 0; j < 500; j++) {
-        resultado[linha][j] = 0;
-        for(int k = 0; k < 500; k++) {
-            resultado[linha][j] += m1[linha][k] * m2[k][j];
+void multiplicaColuna(int m1[500][500], int m2[500][500], int resultado[500][500], int coluna) {
+    for (int linha = 0; linha < 500; linha++) {
+        resultado[linha][coluna] = 0;
+        for (int k = 0; k < 500; k++) {
+            resultado[linha][coluna] += m1[linha][k] * m2[k][coluna];
         }
     }
+
 }
+
 
 int main() {
 
@@ -28,17 +30,19 @@ int main() {
 
     for(int i = 0; i < 500; i++) {
         for(int j = 0; j < 500; j++) {
-            m1[i][j] = distrib(gen);
-            m2[i][j] = distrib(gen);
+            m1[j][i] = distrib(gen);
+            m2[j][i] = distrib(gen);
         }
     }
+
+    
 
     // Vetor de threads
     int num_threads = 500;
     std::vector<std::thread> threads;
 
     for(int i = 0; i < num_threads; i++) {
-        threads.push_back(std::thread(multiplicaLinha, m1, m2, resultado, i));
+        threads.push_back(std::thread(multiplicaColuna, m1, m2, resultado, i));
     }
 
     for(auto &t: threads) {
@@ -49,4 +53,6 @@ int main() {
 
     std::chrono::duration<double> duracao = fim - inicio;
     std::cout << "Tempo de execução: " << duracao.count() << " segundos" << std::endl;
+
+    
 }
